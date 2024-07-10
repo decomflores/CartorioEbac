37eae3d2e56d27dde476df6c1d705d7832a4bd7f
decomflores <75983206+decomflores@users.noreply.github.com>
2024-07-10 10:48:27 -0300

    Opção de Saída
    incluso no menu a opção de saída para o usuário.

--- Biblioteca_Switchcase.c
+++ Biblioteca_Switchcase.c
@@ -145,7 +145,8 @@ int main()//menu
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Excluir nomes\n\n");
+		printf("\t3 - Excluir nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: ");
 		
 		scanf("%d", &opcao);//recebendo a escolha do usuário
@@ -164,6 +165,11 @@ int main()//menu
 			
 			case 3:
 			exclusao();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
